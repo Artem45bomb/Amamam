094dd5c4ef77f165b3b6ab0da38f6a9520a9cb89
geralttop <bluevotinamalibu@gmail.com>
2025-02-12 18:02:38 +0300

    Быстро

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -9,6 +9,7 @@ export default function Order(){
     const [contacts, setContacts]= useState(false)
     const [delivery, setDelivery]= useState(false)
     const [payment, setPayment] = useState(false)
+    const [isCourier, setIsCourier] = useState(true);
     function handleClickContacts(){
         setContacts(!contacts)
     }
@@ -20,7 +21,9 @@ export default function Order(){
     function handleClickPayment(){
         setPayment(!payment)
     }
-
+    function handleCourier(){
+        setIsCourier(!isCourier)
+    }
 
 
     return (
@@ -87,45 +90,46 @@ export default function Order(){
                             </div>
                         </div>
                         <div className={`mt-10 ${!delivery ? 'hidden':''}`}>
-                            <div className="flex gap-10">
+                            <div className="flex gap-10 mb-12">
                                 <p className="text-end text-primary text-base font-gilroy">способ<br/>доставки</p>
                                 <div className="flex flex-col gap-7 flex-1 text-secondary">
-                                    <div className="flex justify-between border-b border-b-secondary">
+                                    <div className="flex justify-between border-b border-b-secondary pb-3">
                                         <div className="flex gap-3">
-                                            <div><input type="radio" name="delivery" id="courier"/></div>
+                                            <div><input type="radio" name="delivery" id="courier" onClick={()=>setIsCourier(true)}/></div>
                                             <label htmlFor="courier">
                                                 <p className="text-base font-gilroy text-primary">курьер</p>
                                                 <p className="text-base font-gilroy text-secondary">доставит в указанный срок</p>
                                             </label>
                                         </div>
-                                        <p className="text-base font-bold font-gilroy">5 BYN</p>
+                                        <p className="text-base font-bold font-gilroy text-primary">5 BYN</p>
                                     </div>
-                                    <div className="flex justify-between border-b border-b-secondary">
+                                    <div className="flex justify-between border-b border-b-secondary pb-3">
                                         <div className="flex gap-3">
-                                            <div><input type="radio" name="delivery" id="self" className={``}/></div>
+                                            <div><input type="radio" name="delivery" id="self" className={``} onClick={()=>setIsCourier(false)}/></div>
                                             <label htmlFor="self">
                                                 <p className="text-base font-gilroy text-primary">самовызов</p>
                                                 <p className="text-base font-gilroy text-secondary">из лавки вкусностей amamam.by</p>
                                             </label>
                                         </div>
-                                        <p className="text-base font-bold font-gilroy">бесплатно</p>
+                                        <p className="text-base font-bold font-gilroy text-primary">бесплатно</p>
                                     </div>
                                 </div>
                             </div>
-                            <div className="flex gap-8 justify-between">
-                                <p className="text-end text-primary text-base font-gilroy">контакты</p>
+
+
+                            <div className={`flex gap-10 mb-12 ${!isCourier? 'hidden':''}`}>
+                                <p className="text-end text-primary text-base font-gilroy">адрес<br/>доставки</p>
                                 <div className="flex flex-col gap-x-7 flex-1 text-secondary">
-                                    <p className="text-xs mb-1">+375 (__) ___ - __ -  __</p>
+                                    <p className="text-xs mb-1">улица, дом, корпус, подъезд, этаж, квартира, домофон</p>
                                     <input type="text" className="bg-transparent border-b border-b-secondary w-full text-primary"/>
-                                    <div className="flex gap-2 my-6">
-                                        <input type="checkbox" name="checkbox" id="rule" className="w-3 h-3"/>
-                                        <label htmlFor="rule">я ознакомился и согласен с Политикой обработки персональных данных и пользовательским соглашением</label>
-                                    </div>
-                                    <div className="flex gap-2">
-                                        <input type="checkbox" name="checkbox" id="ad" className="w-3 h-3"/>
-                                        <label htmlFor="ad">я даю согласие на получение рекламных рассылок в виде e‑mail sms, 
-                                        push или в мессенджерах</label>
-                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={`flex gap-10 justify-between ${!isCourier? 'hidden':''}`}>
+                                <p className="text-end text-primary text-base font-gilroy">дата<br/>доставки</p>
+                                <div className="flex flex-col gap-x-7 flex-1 text-secondary">
+                                    <p className="text-xs mb-1">Дата</p>
+                                    <input type="date" className="bg-transparent border-b border-b-secondary w-full text-primary"/>
                                 </div>
                             </div>
                         </div>
@@ -143,7 +147,26 @@ export default function Order(){
                                 <div className="w-full border border-secondary"></div>
                             </div>
                         </div>
-                        
+                        <div className={`flex flex-col gap-7 flex-1 text-secondary ml-24 mt-10 ${!payment ? 'hidden':''}`}>
+                            <div className="flex justify-between border-b border-b-secondary pb-3">
+                                <div className="flex gap-3">
+                                    <div><input type="radio" name="payment" id="cash"/></div>
+                                    <label htmlFor="courier">
+                                        <p className="text-base font-gilroy text-primary">наличными при получении</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="flex justify-between border-b border-b-secondary pb-3">
+                                <div className="flex gap-3">
+                                    <div><input type="radio" name="payment" id="card" className={``}/></div>
+                                    <label htmlFor="self">
+                                        <p className="text-base font-gilroy text-primary">карта онлайн</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <button className="flex mt-14 justify-center font-gilroy w-full bg-blue-700 items-center py-3 text-xl font-bold text-white">ОПЛАТИТЬ</button>
+
+                        </div>
                     </div>
 
                 </div>
